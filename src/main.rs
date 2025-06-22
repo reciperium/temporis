9928b5cddfecde891b0332f511b64b9654a78682
Santiago Fraire Willemoes <santiwilly@gmail.com>
2025-06-22 11:10:17 +0100

    feat: introduce AppWindow and basic settings support

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 use notify_rust::{Hint, Notification, Urgency};
 
 slint::slint! {
-    export { System, Pomodoro } from "ui/main.slint";
+    export { System, AppWindow } from "ui/main.slint";
 }
 
 fn main() -> Result<(), slint::PlatformError> {
-    let main_window = Pomodoro::new()?;
-    main_window.global::<System>().set_focus_duration(10);
-    main_window.global::<System>().set_long_break_duration(15);
-    main_window.global::<System>().set_short_break_duration(5);
+    let main_window = AppWindow::new()?;
+    // main_window.global::<System>().set_focus_duration(10);
+    // main_window.global::<System>().set_long_break_duration(15);
+    // main_window.global::<System>().set_short_break_duration(5);
 
     main_window.global::<System>().on_notify(|msg| {
         let summary = msg.summary.to_owned();
