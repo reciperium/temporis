33b3af1940dfd2e9c1e38e7f1cbe3200dcb5a316
Santiago Fraire Willemoes <santiwilly@gmail.com>
2025-06-26 10:15:05 +0100

    refactor: create global ExternalSystem and move properties to System

--- src/main.rs
+++ src/main.rs
@@ -4,28 +4,30 @@ use notify_rust::{Hint, Notification, Urgency};
 use temporis::conf::Config;
 
 slint::slint! {
-    export { System, AppWindow } from "ui/main.slint";
+    export { ExternalSystem, AppWindow } from "ui/main.slint";
 }
 
 fn main() -> Result<(), slint::PlatformError> {
     let config = Config::new().expect("config should be loaded correctly");
     let main_window = AppWindow::new()?;
     main_window
-        .global::<System>()
+        .global::<ExternalSystem>()
         .set_focus_duration(config.focus_duration);
     main_window
-        .global::<System>()
+        .global::<ExternalSystem>()
         .set_long_break_duration(config.long_break_duration);
     main_window
-        .global::<System>()
+        .global::<ExternalSystem>()
         .set_short_break_duration(config.short_break_duration);
-    main_window.global::<System>().set_sessions(config.sessions);
+    main_window
+        .global::<ExternalSystem>()
+        .set_sessions(config.sessions);
 
     let shared_cfg = Rc::new(RefCell::new(config));
 
     let cfg_clone1 = shared_cfg.clone();
     main_window
-        .global::<System>()
+        .global::<ExternalSystem>()
         .on_save_focus(move |value: i32| {
             let mut cfg_mut = cfg_clone1.borrow_mut();
             cfg_mut.focus_duration = value;
@@ -37,7 +39,7 @@ fn main() -> Result<(), slint::PlatformError> {
 
     let cfg_clone2 = shared_cfg.clone();
     main_window
-        .global::<System>()
+        .global::<ExternalSystem>()
         .on_save_short_break(move |value| {
             let mut cfg_mut = cfg_clone2.borrow_mut();
             cfg_mut.short_break_duration = value;
@@ -49,7 +51,7 @@ fn main() -> Result<(), slint::PlatformError> {
 
     let cfg_clone3 = shared_cfg.clone();
     main_window
-        .global::<System>()
+        .global::<ExternalSystem>()
         .on_save_long_break(move |value| {
             let mut cfg_mut = cfg_clone3.borrow_mut();
             cfg_mut.long_break_duration = value;
@@ -61,7 +63,7 @@ fn main() -> Result<(), slint::PlatformError> {
 
     let cfg_clone4 = shared_cfg.clone();
     main_window
-        .global::<System>()
+        .global::<ExternalSystem>()
         .on_save_sessions(move |value| {
             let mut cfg_mut = cfg_clone4.borrow_mut();
             cfg_mut.sessions = value;
@@ -71,7 +73,7 @@ fn main() -> Result<(), slint::PlatformError> {
             }
         });
 
-    main_window.global::<System>().on_notify(|msg| {
+    main_window.global::<ExternalSystem>().on_notify(|msg| {
         let summary = msg.summary.to_owned();
         let body = msg.body.to_owned();
         let res = Notification::new()
