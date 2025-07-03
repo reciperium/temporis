8d8b0947c8a97d940989d039616b89f34dd6af7b
Santiago Fraire Willemoes <santiwilly@gmail.com>
2025-07-03 12:10:26 +0100

    feat: add dummy macos implementation for progress

---
+++ src/platform/macos/mod.rs
@@ -0,0 +1,22 @@
+use crate::progress::{ProgressError, ProgressIntegration};
+
+#[derive(Debug, Default, Clone, Copy)]
+struct DummyProgress;
+
+impl ProgressIntegration for DummyProgress {
+    fn set_progress(&mut self, progress: f64) -> Result<(), ProgressError> {
+        Ok(())
+    }
+
+    fn emit(&self) -> Result<(), ProgressError> {
+        Ok(())
+    }
+
+    fn stop(&mut self) -> Result<(), ProgressError> {
+        Ok(())
+    }
+}
+
+pub fn get_progress_integration() -> impl ProgressIntegration {
+    progress::UnityLauncher::default()
+}
--- src/platform/mod.rs
+++ src/platform/mod.rs
@@ -2,3 +2,8 @@
 #[path = "linux/mod.rs"]
 mod platform;
 pub use platform::*;
+
+#[cfg(target_os = "macos")]
+#[path = "macos/mod.rs"]
+mod platform;
+pub use platform::*;
