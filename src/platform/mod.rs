e411ef3c91af5289e4fbdfe333e9441ea58c2cf8
Santiago Fraire Willemoes <santiwilly@gmail.com>
2025-07-23 17:49:55 +0100

    fix: warnings when importing modules

--- src/platform/mod.rs
+++ src/platform/mod.rs
@@ -1,9 +1,11 @@
 #[cfg(target_os = "linux")]
 #[path = "linux/mod.rs"]
 mod platform;
+#[cfg(target_os = "linux")]
 pub use platform::*;
 
 #[cfg(target_os = "macos")]
 #[path = "macos/mod.rs"]
 mod platform;
+#[cfg(target_os = "macos")]
 pub use platform::*;
